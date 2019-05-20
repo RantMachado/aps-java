43681cd2ffe796083640e5be0af6edd1a380558f
Patrick de Souza <patrick.dorbacao@ingresso.com>
2019-05-19 21:47:34 -0300

    Preparando o backend da aplicação

--- ConversorDeIp/src/java/pacote/conversorIp.java
+++ ConversorDeIp/src/java/pacote/conversorIp.java
@@ -5,12 +5,17 @@
  */
 package pacote;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
 
 /**
  *
@@ -18,69 +23,49 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class conversorIp extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private String apiGetDataIpUrl = "http://subnet.im/%s";
+    private String apiMethod = "GET";
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String ip = request.getParameter("conversor");
+        String dataIp = getDataIp(ip);
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet conversorIp</title>");            
+            out.println("<title>Obter informações IP</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet conversorIp at " + request.getContextPath() + "</h1>");
+            out.println(dataIp);
             out.println("</body>");
             out.println("</html>");
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    private String getDataIp(String ip) throws ServletException, IOException {
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        URL obj = new URL(String.format(apiGetDataIpUrl,ip));
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod(apiMethod);
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))) {
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+        }
+        return response.toString();
+    }
 }
